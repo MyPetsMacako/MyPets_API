e2789585497d95bcbfeee490b98474b46216be03
Iván Obejo <ivanobejo@hotmail.es>
2020-02-27 13:33:12 +0100

    Edited Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -2,7 +2,21 @@
 namespace App\Http\Middleware;
 Use Closure;
 
+
 class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTIONS")
+            ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
+
+    }
+}
+
+
+/* class Cors
 {
     public function handle($request, Closure $next)
     {
@@ -23,5 +37,5 @@ class Cors
         return $response;
 
     }
-}
+} */
 
