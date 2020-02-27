b2b62c7e4d3b17805360e732badd51e11c7db235
Iván Obejo <ivanobejo@hotmail.es>
2020-02-27 13:43:27 +0100

    Edited cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -11,7 +11,6 @@ class Cors
             ->header('Access-Control-Allow-Origin', "*")
             ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTIONS")
             ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
-
     }
 }
 
