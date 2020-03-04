39af14c1393fe95533bb6c72a20dd5ea760486f0
Iván Obejo <ivanobejo@hotmail.es>
2020-03-04 11:13:32 +0100

    Posible fix

--- app/Pet.php
+++ app/Pet.php
@@ -48,54 +48,36 @@ class Pet extends Model
 
 public function register(Request $request)
     {
+        if (($request->user_id) == NULL) {
+            $request_token = $request->header('Authorization');
+            $token = new token();
+            $decoded_token = $token->decode($request_token);
+            $user_email = $decoded_token->email;
+            $user = User::where('email', '=', $user_email)->first();
+            $user_id = $user->id;
 
-            if (($request->user_id) == NULL) {
-                try {
-                    $request_token = $request->header('Authorization');
-                    $token = new token();
-                    $decoded_token = $token->decode($request_token);
-                    $user_email = $decoded_token->email;
-                    $user = User::where('email', '=', $user_email)->first();
-                    $user_id = $user->id;
-                    $pet = new self();
-                    $pet->user_id = $user_id;
-                    $pet->name = $request->name;
-                    $pet->species = $request->species;
-                    $pet->breed = $request->breed;
-                    $pet->color = $request->color;
-                    $pet->weight = $request->weight;
-                    $pet->birth_date = $request->birth_date;
-                    $pet->save();
-                    $petId = $pet->id;
-                    return $petId;
-                } catch (\Throwable $th) {
-                    return response()->json([
-                        "qrContent" => "mal"
-                    ],200);
-                }
-                    $pet = new self();
-                    $pet->user_id = $user_id;
-                    $pet->name = $request->name;
-                    $pet->species = $request->species;
-                    $pet->breed = $request->breed;
-                    $pet->color = $request->color;
-                    $pet->weight = $request->weight;
-                    $pet->birth_date = $request->birth_date;
-                    $pet->save();
-                    if ($request->image != NULL)
-                    {
-                        $photo = Storage::putFileAs('Pets', new File($request->image), "$user->id$pet->name.jpg");
-                        $pet->photo = $photo;
-                    }
-                    if ($request->document != NULL)
-                    {
-                        $document = Storage::putFileAs('Documents', new File($request->document), "$user->id$pet->name.pdf");
-                        $pet->document = $document;
-                    }
-                    $petId = $pet->id;
+            $pet = new self();
+            $pet->user_id = $user_id;
+            $pet->name = $request->name;
+            $pet->species = $request->species;
+            $pet->breed = $request->breed;
+            $pet->color = $request->color;
+            if ($request->image != NULL)
+            {
+                $photo = Storage::putFileAs('Pets', new File($request->image), "$user->id$pet->name.jpg");
+                $pet->photo = $photo;
             }
-
-         if ($request->user_id != NULL) {
+            if ($request->document != NULL)
+            {
+                $document = Storage::putFileAs('Documents', new File($request->document), "$user->id$pet->name.pdf");
+                $pet->document = $document;
+            }
+            $pet->weight = $request->weight;
+            $pet->birth_date = $request->birth_date;
+            $pet->save();
+            $petId = $pet->id;
+            return($petId);
+        } else {
 
             $user = User::where('id', '=', $request->user_id)->first();
 
